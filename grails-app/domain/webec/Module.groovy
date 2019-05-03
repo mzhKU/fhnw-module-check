cdffd16ecb0eb0bd409a5a5f66edeb87927d0817
Martin Hediger <ma.hed@bluewin.ch>
2019-05-03 09:07:02 +0200

    Module and Professor domain clean up

--- grails-app/domain/webec/Module.groovy
+++ grails-app/domain/webec/Module.groovy
@@ -7,20 +7,13 @@ class Module {
     static constraints = {
     }
 
-    Professor professor
     String title
-    int votes
 
-    def upvote() {
-        votes += 1
-    }
 
     // A module cannot exist without a title
     // Only modules which are tought are rated
     // A module can have zero ratings
     static contraints = {
         title unique:true
-        title blank:false
-        professor blank:false
     }
 }
