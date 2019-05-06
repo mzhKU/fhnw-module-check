5d11fc8f81139b4e2d80758579ba51155f19c2f3
Martin Hediger <ma.hed@bluewin.ch>
2019-05-06 11:43:35 +0200

    List Modules in index controller

--- grails-app/controllers/webec/AppController.groovy
+++ grails-app/controllers/webec/AppController.groovy
@@ -3,11 +3,15 @@ package webec
 class AppController {
 
     def index() {
-        // render view: 'index', model: [student: "Toni", ratings: Rating.all]
+
+        for(Module m: Module.all) {
+            System.out.println(m.title)
+        }
 
         // 'respond' can return different response formats: JSON, XML, ...
         // '[...]' is the returned model
-        respond([student: "Toni", modules: Module.all,
+        respond([student:              "Toni",
+                 modules:              Module.all,
                  totalNumberOfModules: Module.all.size()])
     }
 }
